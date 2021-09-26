14206c2d1db98ddb802393b81284844b671e7c23
sumeghasetia <sumeghasetia@gmail.com>
2021-09-26 17:59:30 +0530

    Update SQL_Project.sql

--- SQL_Project.sql
+++ SQL_Project.sql
@@ -1,8 +1,10 @@
 
---Q1:  We want to run an Email Campaigns for customers of Store 2 (First, Last name, and Email address of customers from Store 2)	
+--Q1:  We want to run an Email Campaigns for customers of Store 2 (First, Last name, and Email address of customers from Store 2)
+	
 Select first_name, last_name, email from customer where store_id = 2;
 
---Q2:  List of the movies with a rental rate of 0.99$
+--Q2:  List of the movies with a rental rate of 0.99$
+
 select film_id, title, description reantal_rate from film where rental_rate = 0.99;
 
 --Q3: Your objective is to show the rental rate and how many movies are in each rental rate categories
@@ -30,7 +32,8 @@ select sq.* from (select store_id, rating, count(f.film_id) as movieCount,
 select customer_id, first_name, last_name, email from customer where active = TRUE;
 
 
---Q7. List of films by Film Name, Category, Language
+--Q7. List of films by Film Name, Category, Language
+
 select f.film_id, title, c.name, l.name 
 from film f 
 	left join film_category fc on f.film_id = fc.film_id 
@@ -59,7 +62,8 @@ group by f.film_id, f.title
 order by 3 desc limit 10;
  
 
---Q10: Most Spending Customer so that we can send him/her rewards or debate points
+--Q10: Most Spending Customer so that we can send him/her rewards or debate points
+
 select sq.* from 
 	(select c.customer_id, c.first_name, c.last_name, c.email, SUM(p.amount) cutomerSpent, 
 		RANK() OVER (ORDER BY SUM(p.amount) desc) AS RowRank 
@@ -95,7 +99,8 @@ group by Month(rental_date), year(rental_date);
 
 --Q14 & 15: 
 --14.Which date the first movie was rented out?
---15.Which date the last movie was rented out?
+--15.Which date the last movie was rented out?
+
 select min(date(rental_date)) firstRentalDate, max(date(rental_date)) lastRentalDate from rental;
  
 
@@ -106,13 +111,15 @@ select f.film_id, f.title, max(r.rental_date) lastRentalDate, min(r.rental_date)
 	left join rental r on i.inventory_id = r.inventory_id 
 group by f.film_id, f.title limit 10;
  
---Q17: What is the Last Rental Date of every customer?
+--Q17: What is the Last Rental Date of every customer?
+
 select c.customer_id, c.first_name, min(rental_date) firstRentalDate, max(rental_date) lastRentalDate from customer c 
 	left join rental r on r.customer_id = c.customer_id 
 group by c.customer_id, c.first_name limit 10;
  
 
---Q18: What is our Revenue Per Month?
+--Q18: What is our Revenue Per Month?
+
 select monthname(rental_date), sum(amount) RevenuePerMonth from rental r 
 	left join payment p on r.rental_id = p.rental_id 
 group by monthname(rental_date);
@@ -134,7 +141,8 @@ select monthname(rental_date) RentalMonth, year(rental_date) RentalYear, count(d
 group by monthname(rental_date), year(rental_date) order by 2 desc;
  
 
---Q21: Number of Rentals in Comedy, Sports, and Family
+--Q21: Number of Rentals in Comedy, Sports, and Family
+
 select count(distinct rental_id) NumRentals, c.name 
 from rental r 
 	left join inventory i on r.inventory_id = i.inventory_id 
@@ -197,7 +205,11 @@ having count(r.rental_id)>=30;
  
 --Q26: Reward Users who are also active
 
-select r.customer_id, c.first_name, c.last_name, c.email, count(r.rental_id) NumOfRentals from rental r left join customer c on r.customer_id = c.customer_id where c.active = 1 group by r.customer_id,  c.first_name, c.last_name, c.email having count(r.rental_id)>=30;
+select r.customer_id, c.first_name, c.last_name, c.email, count(r.rental_id) NumOfRentals 
+from rental r 
+	left join customer c on r.customer_id = c.customer_id 
+where c.active = 1 
+group by r.customer_id,  c.first_name, c.last_name, c.email having count(r.rental_id)>=30;
  
 
 --Q27: All Rewards Users with Phone
